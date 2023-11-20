ddf95ce2e750f15c97bda34a9f04880a5c66dca4
sayam9901 <sayamkhan9560621912@gmail.com>
2023-11-20 22:05:37 +0530

    seeting the hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { MongoClient } from "mongodb";
+export default async function handler(req, res) {
+   
+const data = req.body;
+const client = await MongoClient.connect('mongodb+srv://mayanksharma:Mayank1029@cluster0.ymj7rwo.mongodb.net/')
+const db = client.db();
+const collection = db.collection('Items');
+const insertedItem = await collection.insertOne(data);
 
-export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
+
+res.json(insertedItem)
+
 }
